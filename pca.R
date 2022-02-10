049aa49ed834ccba712eeb793525aa3e2de860d6
Sterling Fearing <sterlingfearing@icloud.com>
2022-02-09 16:16:57 -0800

    reorganizing file structure

---
+++ pca.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+
+raw <- read_csv("code/data/tempLawIndex.csv")
+
+library(factoextra) # principle component analysis
+library(ggbiplot)
+
+data(raw)
+indexActive <- raw[-c(1:9)] # select only indexed variables, take out states b/c character 
+row.names(indexActive) <- raw %>% pull(state)
+prcomp(indexActive, scale = TRUE)  %>% # pca, scale = T, standardizes ranges 
+summary()
+
+ggbiplot(prcomp(indexActive, scale = FALSE), labels = rownames(indexActive))
+
